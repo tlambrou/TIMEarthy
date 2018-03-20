1943860d2d9bbf86f62d563bd7981733d52d20ce
Tassos Lambrou <lambrou@gmail.com>
2018-03-20 13:40:01 -0700

    Added new logo and changed name to TimeOut

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <img src="/img/clock.gif" width="100px" height="100px"/>
-        <img src="/img/logo.png" />
-        <img src="/img/just_earth_150.gif" width="100px" height="100px"/>
+        <img src="/img/TimeOut.png" />
       </div>
     )
   }
